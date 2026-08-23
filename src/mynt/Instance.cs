f8d7bb86a716959d4629e371302e544df9c713b8
Aqua <aqua@aquagoose.com>
2026-08-23 22:13:22 +0100

    start instances

---
+++ src/mynt/Instance.cs
@@ -0,0 +1,17 @@
+﻿namespace mynt;
+
+/// <summary>
+/// The base instance for a mynt context.
+/// </summary>
+public abstract class Instance : IDisposable
+{
+    /// <summary>
+    /// Gets if this <see cref="Instance"/> is disposed.
+    /// </summary>
+    public abstract bool IsDisposed { get; protected set; }
+
+    /// <summary>
+    /// Dispose of this <see cref="Instance"/>.
+    /// </summary>
+    public abstract void Dispose();
+}
